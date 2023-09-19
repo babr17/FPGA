6f6a9ee11942fbb1a22940cba6823f5c2c2ae7a9
bruhdyaga <124033009+bruhdyaga@users.noreply.github.com>
2023-09-19 17:26:07 +0300

    pet
    Разработали с нуля маленький блок модулей и удачно отладили на тестбенче

--- simple/tb/tb.sv
+++ simple/tb/tb.sv
@@ -7,14 +7,14 @@ logic [3:0] SW;
 logic [6:0] HEX0;
 
 
-simple testbench(.SW(SW), .INV(INV) , .HEX0(HEX));
+simple testbench(.SW(SW),.HEX0(HEX0));
 
 
 initial 
 begin
- SW = 4'b0000;
- HEX0 = 6'b000000;
- INV = 4'b0000;
+ SW = 4'b0001;
+ HEX0 = 6'b010000;
+
 
 # 15;
 
@@ -22,6 +22,10 @@ SW [3:0] = 4'b0101;
 $display ("babr");
 
 # 20;
+SW [3:0] = 4'b1101; 
+$display ("babr");
+
+# 25;
 
 end
 
